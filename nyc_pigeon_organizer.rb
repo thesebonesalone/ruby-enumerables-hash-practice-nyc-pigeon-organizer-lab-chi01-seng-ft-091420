a6ebaffd9f79a4fad08162bbc8382c34830b6536
Willem A Duiker <duikhead2@gmail.com>
2020-08-22 17:56:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  name_array = []
+  count = 0
+  data[:gender].each do |level|
+    level.each do |level2|
+      level2.each do |level3|
+      name_array[count] = level3
+      count += 1
+      end
+    end
+  end
+  name_array = name_array.uniq
+  puts name_array
 end
