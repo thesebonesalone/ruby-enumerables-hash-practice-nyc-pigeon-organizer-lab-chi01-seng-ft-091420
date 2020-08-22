946fefda89248a79af266d5143c5587767fc47e0
Willem A Duiker <duikhead2@gmail.com>
2020-08-22 18:04:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,5 @@
 def nyc_pigeon_organizer(data)
   name_array = []
   count = 0
-  flat = data.flatten
-  flat.each do |index|
-    index.each do |index2|
-      name_array[count] = index2
-      count += 1
-    end
-  end
-  puts name_array
+  name_array = data.values
 end
